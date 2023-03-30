f13a3c6097304fbb188cdd876d20ab792d35e838
ChrisVerdida <christian.verdida123@gmail.com>
2023-03-30 15:40:20 +0800

    Adding the Card Text Section in home and Responsive CSS

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// ----------------- CARD TEXT SECTION START -----------------
+// ----------------- CARD TEXT SECTION START -----------------
+// ----------------- CARD TEXT SECTION START -----------------
+let arrivalCard = document.querySelectorAll('.new-arrival-card');
+
+let move = (arrivalCard[0].offsetWidth + 20) * 3;
+
+let newArrival = setInterval(() => {
+    if (move >= ((arrivalCard[0].offsetWidth + 20) * (arrivalCard.length / 1))) {
+        move = 0;
+    }
+
+    for (let i = 0; i < arrivalCard.length; i++) {
+        arrivalCard[i].style.transform = `translateX(-${move}px)`;
+    }
+
+    move += (arrivalCard[0].offsetWidth + 20) * 3;
+
+}, 3000);
+// ----------------- CARD TEXT SECTION END -----------------
+// ----------------- CARD TEXT SECTION END -----------------
+// ----------------- CARD TEXT SECTION END -----------------
+
+
